06cbf7a6a8b1407e06d75cdb17f314d24db4dde6
espiler <monkeysrawesome@gmail.com>
2014-11-18 19:18:34 -0800

    converted to Express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,59 @@
-/* Import node's http module: */
 var http = require("http");
-var handler = require("./request-handler.js").requestHandler;
+var handler = require("./request-handler.js");
+
+
+var express = require("express");
+var bodyParser = require('body-parser')
+var app = express();
+app.set('trust proxy', 'loopback')
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Content-Type", "application/json")
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+// app.use(express.json());       // to support JSON-encoded bodies
+// app.use(express.urlencoded()); // to support URL-encoded bodies
+app.listen(3000);
+app.get('/', function(req, res) {
+  res.send('This is the default room')
+});
+
+
+app.get('/classes/', function(req, res) {
+  res.send(handler.getAllMessages(req,res))
+});
+
+//Some other page
+app.get('/classes/room1', function(req, res) {
+  res.send('Hello World!')
+});
+
+//I can respond to a form post
+app.post('/classes', function(req, res) {
+  console.log(req.body);
+  handler.addNewMessage(req, res)
+  res.send("posted!")
+});
+
+// var port = 3000;
+
+// var ip = "127.0.0.1";
+
+// var server = http.createServer(handler);
+
+// console.log("Listening on http://" + ip + ":" + port);
+
+// server.listen(port, ip);
+
+//Express Stuff//
+
+/* Import node's http module: */
 
 
 // Every server needs to listen on a port with a unique number. The
@@ -8,13 +61,11 @@ var handler = require("./request-handler.js").requestHandler;
 // normally already claimed by another server and/or not accessible
 // so we'll use a standard testing port like 3000, other common development
 // ports are 8080 and 1337.
-var port = 3000;
+
 
 // For now, since you're running this server on your local machine,
 // we'll have it listen on the IP address 127.0.0.1, which is a
 // special address that always refers to localhost.
-var ip = "127.0.0.1";
-
 
 
 // We use node's http module to create a server.
@@ -23,9 +74,7 @@ var ip = "127.0.0.1";
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handler);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+
 
 // To start this server, run:
 //
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,127 +1,18 @@
 var exports = module.exports = {};
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 var url = require("url");
 var queryString = require("querystring");
-// var statusCode;
-// var headers;
+var statusCode;
+var headers;
 var classesRegEx;
 var newMessages;
 
-var requestHandler = function(request, response) {
+var getAllMessages = exports.getAllMessages = function(req, res) {
+  return JSON.stringify(messages);
+}
 
-  var theUrl = url.parse( request.url );
-
-  var queryObj = queryString.parse( theUrl.query );
-
-
-  // var obj = JSON.parse( queryObj.format );
-
-
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-
-
-
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  // headers['Content-Type'] = "application/json";
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-
-  classesRegEx = /classes/;
-
-  if (request.url.match(classesRegEx)) {
-
-    if (request.method === 'GET') {
-      var room = theUrl.pathname.substring(9);
-      response.writeHead(statusCode, headers);
-      if(!room || room === 'messages'){
-        response.end(JSON.stringify(messages));
-      } else {
-        newMessages = {
-          results: messages.results.filter(function(item){
-            return item.roomname === room;
-          })
-        }
-        response.end(JSON.stringify(newMessages));
-      }
-    }
-    if (request.method === 'POST' || request.method === 'OPTIONS') {
-      statusCode = 201;
-      console.log(statusCode)
-      response.writeHead(statusCode, headers);
-      request.on('data', function (chunk) {
-        messages.results.unshift(JSON.parse(chunk));
-      })
-      response.end('Posted')
-    }
-  } else {
-    statusCode = 404;
-    response.writeHead(statusCode, headers);
-    response.end('Not Found')
-  }
-
-};
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10,
-  "Content-Type": "application/json" // Seconds.
-};
+var addNewMessage = exports.addNewMessage = function(req, res) {
+  messages.results.unshift(req.body);
+}
 
 var messages = {
   results: [
@@ -132,7 +23,7 @@ var messages = {
       roomname: "lobby",
       text: "hello",
       updatedAt: "2013-10-07T16:22:03.280Z",
-      username: "gary"
+      username: "Jack"
     },
     {
       message: "2nd message",
@@ -148,4 +39,64 @@ var messages = {
 
 
 
-exports.requestHandler = requestHandler;
+// var requestHandler = function(request, response) {
+
+//   var theUrl = url.parse( request.url );
+
+//   var queryObj = queryString.parse( theUrl.query );
+//   // var obj = JSON.parse( queryObj.format );
+
+//   console.log("Serving request type " + request.method + " for url " + request.url);
+
+//   statusCode = 200;
+//   headers = defaultCorsHeaders;
+//   classesRegEx = /classes/;
+
+//   if (theUrl.path === '/') {
+//     response.writeHead(statusCode, headers);
+//     response.end(
+//     //serve up index.html
+//     )
+//   } else if (request.url.match(classesRegEx)) {
+//     if (request.method === 'GET') {
+//       response.writeHead(statusCode, headers);
+//       response.end(JSON.stringify(messages));
+//     }
+//     if (request.method === 'POST' || request.method === 'OPTIONS') {
+//       statusCode = 201;
+//       response.writeHead(statusCode, headers);
+//       request.on('data', function (chunk) {
+//         messages.results.unshift(JSON.parse(chunk));
+//       })
+//       response.end('Posted')
+//     }
+//   } else {
+//     statusCode = 404;
+//     response.writeHead(statusCode, headers);
+//     response.end('Not Found')
+//   }
+// };
+
+// var defaultCorsHeaders = {
+//   "access-control-allow-origin": "*",
+//   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+//   "access-control-allow-headers": "content-type, accept",
+//   "access-control-max-age": 10,
+//   "Content-Type": "application/json"
+// };
+
+
+// exports.requestHandler = requestHandler;
+
+
+      // var room = theUrl.pathname.substring(9);
+      // if(!room || room === 'messages'){
+      // }
+      // else {
+      //   newMessages = {
+      //     results: messages.results.filter(function(item){
+      //       return item.roomname === room;
+      //     })
+      //   }
+      //   response.end(JSON.stringify(newMessages));
+      // }
